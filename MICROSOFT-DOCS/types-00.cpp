f56f893ef85eff0f24964eb4e4dd9ab12950b80f
Mack Octavian <maestrooctavian@gmail.com>
2023-11-23 13:22:29 +0300

    Learned more about types on microsoft C++ docs

---
+++ MICROSOFT-DOCS/types-00.cpp
@@ -0,0 +1,12 @@
+/**
+ *  C++ type system a learning from microsoft C++ Docs
+ *  Reference: https://learn.microsoft.com/en-us/cpp/cpp/cpp-type-system-modern-cpp?view=msvc-170
+*/
+#include <iostream>
+
+int main() {
+    auto name = "Mack O."; //Declaring a variable and let compiler deduce the type
+    const double PI = 3.1415; // Value pf const type can't be modified after it's initilization
+
+    return 0;
+}
